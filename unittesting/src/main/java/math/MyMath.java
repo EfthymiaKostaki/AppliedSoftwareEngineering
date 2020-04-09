26d1655fb04318a3a290860a0a1893768d1b1bc2
EfthymiaKostaki <kostakiefthymia@gmail.com>
2020-04-09 14:07:38 +0300

    Add MyClass with method for calculating the factorial

---
+++ unittesting/src/main/java/math/MyMath.java
@@ -0,0 +1,22 @@
+package math;
+
+/**
+ * An class that provides methods for the 
+ * math operations. Specifically this class includes a method 
+ * for calculating the factorial of a number.
+ * @author EfthymiaKostaki
+ */
+public class MyMath {
+ 
+	public long factorial(int n) {
+		if (n < 0 || n > 20) {
+			throw new IllegalArgumentException("n cannot be less than 0 or greater than 20");
+		} else if (n == 1){
+		    return n;
+		} else if (n == 0){
+			return 1;
+		} else {	
+			return n * factorial(n-1);
+		}
+	}
+}
