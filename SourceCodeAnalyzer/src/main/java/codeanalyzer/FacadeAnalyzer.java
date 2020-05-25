d296407d1706707b44fb22eeee95e80376619979
EfthymiaKostaki <kostakiefthymia@gmail.com>
2020-05-25 22:28:48 +0300

    Add documentation in FacadeAnalyzer

--- SourceCodeAnalyzer/src/main/java/codeanalyzer/FacadeAnalyzer.java
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/FacadeAnalyzer.java
@@ -11,12 +11,20 @@ import codeanalyzer.sourceCodeAnalyzer.SourceCodeAnalyzer;
 import codeanalyzer.sourceFileReader.FileReaderFactory;
 import codeanalyzer.sourceFileReader.SourceFileReader;
 
+/**
+ * A class which applies the Facade design pattern for analyzing the 
+ * source code of the project using different metrics. 
+ * @author EfthymiaKostaki
+ * @version 1.0
+ * @since   2020-05-25 
+ */
 public class FacadeAnalyzer {
 	String filepath;
 	String sourceCodeAnalyzerType;
 	String sourceFileLocation;
 	String outputFilePath;
 	String outputFileType;
+	
 	
 	public FacadeAnalyzer(String filepath, String sourceCodeAnalyzerType, 
 			String sourceFileLocation,
@@ -28,6 +36,12 @@ public class FacadeAnalyzer {
 		this.outputFileType = outputFileType;
 	}
 	
+	/**
+	 * analyze() is responsible for the main part of the project since it handles all 
+	 * communication with the subpackages.
+	 *
+	 * @exception IOException when there is a problem with the file being read. 
+	 */
 	public void analyze() throws IOException {
 		AnalyzerFactory analyzerFactory = new AnalyzerFactory();
 		FileReaderFactory fileReaderFactory = new FileReaderFactory();
